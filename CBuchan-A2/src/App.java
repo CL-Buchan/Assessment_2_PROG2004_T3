f64d8a302e9f2d5645fee9631303e010f898f662
CL-Buchan <clathanbuchan@gmail.com>
2025-07-29 21:00:10 +1000

    Add files via upload
    Initial commit of Assessment_2 project, called CBuchan-A2. Includes the main Java file and supporting classes for program logic.

---
+++ CBuchan-A2/src/App.java
@@ -0,0 +1,12 @@
+/*
+ * Park Rides Visitor Management System (PRVMS) Main Logic
+ * 
+ * Assessment 2
+ * Author: Callam Buchan
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
